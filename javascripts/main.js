833820adef5e372837238700d2de1837631c7522
gib <gibbowden@gmail.com>
2017-09-15 00:32:37 -0500

    structure complete, need to add data

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const solarSystem = require("./solarSystem");
+
+const planetsContainer = document.getElementById("planets-container");
+
+solarSystem.planets.forEach( (planet) => {
+    solarSystem.outputTo(planet, planetsContainer);
+});
---
+++ javascripts/mercury.js
@@ -0,0 +1,14 @@
+"use strict"; 
+
+const planet = {
+    name: "Mercury",
+    discovered: "Prehistory",
+    mass: "3.3011x10^23 kg",
+    volume: "6.083x10^10",
+    orbit: "57 million km",
+    atmosphere: ["oxygen", "sodium", "hydrogen", "helium", "potassium"],
+    satellites: "none",
+    probes: ["Mariner 10", "MESSENGER"]
+};
+
+module.exports = planet; 
